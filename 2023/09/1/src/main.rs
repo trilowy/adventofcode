4665b109642a412bcb3a263358ef25d9f7c5ec6f
Trilowy <49493635+trilowy@users.noreply.github.com>
2023-12-09 17:59:19 +0100

    2023/12/09 part 1

---
+++ 2023/09/1/src/main.rs
@@ -0,0 +1,70 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Read},
+};
+
+fn main() {
+    let file = File::open("./2023/09/1/input.txt").expect("opening input file");
+    println!("{}", process(file));
+}
+
+fn process(file: impl Read) -> i64 {
+    BufReader::new(file)
+        .lines()
+        .flatten()
+        .map(find_next_value)
+        .sum()
+}
+
+fn find_next_value(line: String) -> i64 {
+    let mut all_zeros = true;
+
+    let mut values: Vec<i64> = line
+        .split_whitespace()
+        .map(|value| {
+            let value = value.parse().unwrap();
+            if value != 0 {
+                all_zeros = false;
+            }
+            value
+        })
+        .collect();
+
+    let mut last_values = Vec::new();
+
+    while !all_zeros {
+        last_values.push(*values.last().unwrap());
+
+        all_zeros = true;
+
+        values = values
+            .windows(2)
+            .map(|tuple| {
+                let value = tuple[1] - tuple[0];
+                if value != 0 {
+                    all_zeros = false;
+                }
+                value
+            })
+            .collect();
+    }
+
+    last_values.into_iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::process;
+    use stringreader::StringReader;
+
+    #[test]
+    fn test_example_result() {
+        let lines = StringReader::new(
+            "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45",
+        );
+
+        assert_eq!(process(lines), 114);
+    }
+}
